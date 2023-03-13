7fb559693854651ea90d1bdc87e7d78af669b5da
CodyRagsdale <119916333+CodyRagsdale@users.noreply.github.com>
2023-03-13 13:39:55 -0400

    Add environmental variable for Atlas connection

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
+//mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Get all movies
 app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
